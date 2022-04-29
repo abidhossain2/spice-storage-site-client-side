1b7509ac6f4db7bbab10ef96462e71c2e59a3572
abidhossain2 <tanvirabid229@gmail.com>
2022-04-29 23:10:21 +0600

    banner added

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from '../Header/Header';
+import Products from '../Products/Products';
 import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className='banner'>
-            <Header></Header>
-            <div className='header-txt'>
-                <h2>Primium Quality <br /> & <br /> Products</h2>
+        <div>
+            <div className='banner'>
+                <Header></Header>
+                <div className='header-txt'>
+                    <h2>Primium Quality <br /> & <br /> Products</h2>
+                </div>
+            </div>
+            <div>
+                <Products></Products>
             </div>
         </div>
     );
---
+++ src/Pages/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Products.css'
+
+const Products = () => {
+    return (
+        <div className='products-section'>
+            <div className='products-heading'>
+                <h2>Products</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
