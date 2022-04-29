3b5a1c9477419a97c0f2cccb3c4b091a21d4186c
abidhossain2 <tanvirabid229@gmail.com>
2022-04-30 02:39:10 +0600

    update button added

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Product.css'
-
+import {MdUpdate} from 'react-icons/md'
 const Product = (props) => {
-    const {name, img, detail, price, quantity, supplier} = props.product;
+    const { name, img, detail, price, quantity, supplier } = props.product;
     return (
-        <div className='product'>
-            <img src={img} alt="" />
-            <p className='name'>Name: {name}</p>
-            <p className='detail'>{detail}</p>
-            <p className='price'>Price: {price}</p>
-            <p className='quantity'>Quantity: {quantity}</p>
-            <p className='supplier'>Supplier: {supplier}</p>
-        </div>
+        <>
+            <div className='product'>
+                <img src={img} alt="" />
+                <p className='name'><span>Name:</span> {name}</p>
+                <p className='detail'>{detail}</p>
+                <p className='price'><span>Price:</span> {price}</p>
+                <p className='quantity'><span>Quantity:</span> {quantity}</p>
+                <p className='supplier'><span>Supplier:</span> {supplier}</p>
+                <button>Stoke Update <MdUpdate className='update-icon'></MdUpdate></button>
+            </div>
+        </>
     );
 };
 
