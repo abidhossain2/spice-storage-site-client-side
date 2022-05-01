ed8b791972fd4427816355334b78d6063178a81c
abidhossain2 <tanvirabid229@gmail.com>
2022-05-02 02:32:40 +0600

    register page create

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css'
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-    const createUser = () => {
-        const [email, setEmail] = useState('');
-        const [password, setPassword] = useState('');
+    const backToHome = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleName = e => {
+        e.preventDefault();
+        setName(e.target.value)
+    }
+    const handleEmail = e => {
+        e.preventDefault();
+        setEmail(e.target.value)
+    }
+    const handlePassword = e => {
+        e.preventDefault();
+        setPassword(e.target.value)
+    }
+    const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
+    const createNewUser = e => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(email, password);
+        backToHome('/')
     }
     return (
         <div>
-            <form onSubmit={createUser}>
-                <input type="text" name="name" placeholder="Name" /> <br />
-                <input type="email" name="email" placeholder="Email" /> <br />
-                <input type="password" name="password" placeholder="Password" /> <br />
+            <form onSubmit={createNewUser}>
+                <input type="text"  placeholder="Name" value={name} onChange={handleName}/> <br />
+                <input type="email" placeholder="Email" value={email} onChange={handleEmail}/> <br />
+                <input type="password" placeholder="Password" value={password} onChange={handlePassword}/> <br />
                 <button>Register</button>
             </form>
         </div>
